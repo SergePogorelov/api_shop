aef7161459475e4bb5e614eb9288fc4ca7232b5d
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-10-28 14:29:33 +0300

    created models: Category, Product

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    categories = models.ManyToManyField(
+        Category, related_name="products"
+    )
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.CharField(max_length=200, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    published = models.BooleanField(default=True)
+    deleted = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
